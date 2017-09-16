7a9808f44330bab016cac9cba2baff82a1e011e1
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-09-16 21:30:01 +0530

    array example

---
+++ src/com/atuldwivedi/learn/jse/lang/array/Array01.java
@@ -0,0 +1,15 @@
+package com.atuldwivedi.learn.jse.lang.array;
+
+public class Array01 {
+
+	public static void main(String[] args) {
+		int[] iArr = new int[5];
+		
+		System.out.println(iArr.length);
+		
+		for (int i = 0; i < iArr.length; i++) {
+			System.out.println(iArr[i]);
+		}
+	}
+
+}
